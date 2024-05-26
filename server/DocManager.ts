84b1f81077235c8ab2b1a149a4e83a54fe6965bb
Daniel Franklin <daniel@danielzfranklin.org>
2024-05-26 21:22:37 +0100

    Fix race condition in DocManager

--- server/DocManager.ts
+++ server/DocManager.ts
@@ -4,17 +4,17 @@ import { ServerDoc, ServerDocConfig } from "../core/ServerDoc.ts";
 export class DocManager {
   private _docConfig: ServerDocConfig;
 
-  private _docs = new Map<string, ServerDoc>();
+  private _docs = new Map<string, Promise<ServerDoc>>();
 
   constructor(config: { doc: ServerDocConfig }) {
     this._docConfig = config.doc;
   }
 
-  public async get(docId: string): Promise<ServerDoc> {
+  public get(docId: string): Promise<ServerDoc> {
     let doc = this._docs.get(docId);
     if (!doc) {
       log.info("Creating ServerDoc", { docId });
-      doc = await ServerDoc.load(this._docConfig, docId);
+      doc = ServerDoc.load(this._docConfig, docId);
       this._docs.set(docId, doc);
     }
     return doc;
@@ -23,7 +23,8 @@ export class DocManager {
   public closeDoc(docId: string) {
     const doc = this._docs.get(docId);
     if (!doc) return;
+    log.info("Closing ServerDoc", { docId });
     this._docs.delete(docId);
-    doc.close();
+    doc.then((d) => d.close());
   }
 }
